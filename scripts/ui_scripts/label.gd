74af9733e2cc1e3cfc06d5045c666f950cd9a9dc
nf12pro <rayofaik@gmail.com>
2025-11-03 21:34:15 +0100

    Seed Counter

---
+++ scripts/ui_scripts/label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+func _process(delta: float) -> void:
+	if Global.amount_of_seeds == 1:
+		text = "Remaining Seeds: 5"
+	elif Global.amount_of_seeds == 2:
+		text = "Remaining Seeds: 4"
+	elif Global.amount_of_seeds == 3:
+		text = "Remaining Seeds: 3"
+	elif Global.amount_of_seeds == 4:
+		text = "Remaining Seeds: 2"
+	elif Global.amount_of_seeds == 5:
+		text = "Remaining Seeds: 1"
+	elif Global.amount_of_seeds == 6:
+		text = "No Remaining Seeds"
