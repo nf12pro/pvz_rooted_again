00577a67d30db898bd32188c6d465385464570f3
nf12pro <rayofaik@gmail.com>
2025-11-03 13:02:31 +0100

    Almanac Bug Fixing
    Doomshroom and Plasma Pea Button loading same thing, peashooter button not loading fixeed

--- scripts/ui_scripts/settings/turn_off_mowers.gd
+++ scripts/ui_scripts/settings/turn_off_mowers.gd
@@ -1,4 +1,13 @@
 extends Button
 
+var a_pressed: bool = false
+
 func _on_pressed() -> void:
-	Global.mowers_nerf = true
+	if a_pressed == false:
+		Global.mowers_nerf = true
+		text = "MOWERS OFF"
+		a_pressed = true
+	else:
+		Global.mowers_nerf = false
+		text = "MOWERS ON"
+		a_pressed = false
