37abd96590d42bc5cd159e90b299633836e302d5
nf12pro <rayofaik@gmail.com>
2025-11-04 17:43:30 +0100

    Conehead Zombie Fixed

--- scripts/systems_scripts/lawn_mower.gd
+++ scripts/systems_scripts/lawn_mower.gd
@@ -27,6 +27,7 @@ func _physics_process(delta):
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Zombie"):
 		if active == false:
+			print("Detected")
 			if body.has_method("take_damage"):
 				body.take_damage(damage)
 			active = true
