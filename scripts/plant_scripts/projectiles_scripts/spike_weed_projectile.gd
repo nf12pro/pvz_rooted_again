b79d18e7b4e1d84195529f39244f1b1e3a8390fd
nf12pro <rayofaik@gmail.com>
2025-11-10 11:32:04 +0100

    Save
    Progress Randomly Track

---
+++ scripts/plant_scripts/projectiles_scripts/spike_weed_projectile.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+#region Variables
+@export var speed: float = 0.00000000000000000000000000000000000000000000000000000000000000000000000000000001
+@export var damage: int = 20
+var recent: bool = true
+#endregion
+
+func _ready() -> void:
+	if Global.pea_boost == 1:
+		damage = 30
+	elif Global.pea_boost == 2:
+		damage = 40
+	elif Global.pea_boost == 3:
+		damage = 60
+	await get_tree().create_timer(0.02).timeout
+	recent = false
+
+#region Layer Settings
+func layer_setting():
+	collision_layer = 3  # Layer 1 and Layer 5
+	collision_mask = 1
+#endregion
+
+#region Bullet Speed/Movement
+func _physics_process(delta):
+	position.x += speed * delta
+	delete_pea()
+#endregion
+
+#region Do Damage
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Zombie"):
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
+			queue_free() 
+#endregion
+
+#region Delete Pea
+func delete_pea():
+	if position.x >= 1500:
+		queue_free()
+#endregion
