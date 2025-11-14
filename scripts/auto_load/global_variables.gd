223d2ba0cb0e0503f282684879b7483c4bc72a82
nf12pro <rayofaik@gmail.com>
2025-11-14 08:59:31 +0100

    Added Credits
    In game added credits to me and artist

--- scripts/auto_load/global_variables.gd
+++ scripts/auto_load/global_variables.gd
@@ -26,12 +26,12 @@ var plasma_pea_selected: bool = false
 var snow_pea_selected: bool = false
 var frost_pea_selected: bool = false
 
-var twin_sunflower_unlocked: bool = true
+var twin_sunflower_unlocked: bool = false
 var gatling_gun_unlocked: bool = false
-var plasma_wood_unlocked: bool = true
+var plasma_wood_unlocked: bool = false
 var tall_nut_unlocked: bool = false
-var plasma_pea_unlocked: bool = true
-var frost_pea_unlocked: bool = true 
+var plasma_pea_unlocked: bool = false
+var frost_pea_unlocked: bool = false 
 
 var is_new_open_slot: bool = false
 var new_open_slot: bool = false
--- scripts/ui_scripts/achievements/sun_collector_achievement.gd
+++ scripts/ui_scripts/achievements/sun_collector_achievement.gd
@@ -1,7 +1,6 @@
 extends Control
 
 func _ready() -> void:
-	Global.sun_collector_achievement = true
 	if Global.sun_collector_achievement == true:
 		$".".show()
 	elif Global.sun_collector_achievement == false:
