85c2fb3107f3688ba173f51efdd79b94e493298f
nf12pro <rayofaik@gmail.com>
2025-11-14 10:35:09 +0100

    Drafts of Translation

--- scripts/ui_scripts/settings/settings.gd
+++ scripts/ui_scripts/settings/settings.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var language_drop_down: OptionButton = $MarginContainer/VBoxContainer/LanguageButton
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Space") or Input.is_action_just_pressed("A"):
 		get_tree().change_scene_to_file("res://scenes/ui/start_menu.tscn")
+
+func _on_language_selected(index: int) -> void:
+	var language = language_drop_down.get_item_text(index)
+	TranslationServer.set_locale(language)
